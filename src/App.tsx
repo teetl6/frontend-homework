2822177f5b621d523f0fa3557fead8bb0942127b
Teet Laja <teet@viirisepa.ee>
2021-04-13 22:07:24 +0300

    moved components to separate folder for clarity

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./stylesheets/index.scss";
 
 import DropdownLink from "./DropdownLink";
-import DropdownMenu from "./DropdownMenu";
+import DropdownMenu from "./components/DropdownMenu";
 
 const App = () => (
   <nav className="nav">
